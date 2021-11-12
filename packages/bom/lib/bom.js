4cf74404b247e0d5c03c1c8d65d2f0ea1cf6cec9
Liam Wang <skid-scales.0a@icloud.com>
2021-11-12 14:37:12 +0800

    add function openTab

--- packages/bom/lib/bom.js
+++ packages/bom/lib/bom.js
@@ -1,8 +1,20 @@
 'use strict';
 
-module.exports = bom;
+/**
+ * @description 打开新页面
+ * @param {String} url 地址
+ * openTab("https://www.jd.com/");
+ */
+function openTab (url, target = '_blank') {
+    const a = document.createElement('a')
+    a.setAttribute('href', url)
+    a.setAttribute('target', target)
+    a.setAttribute('id', 'd2admin-link-temp')
+    document.body.appendChild(a)
+    a.click()
+    document.body.removeChild(document.getElementById('d2admin-link-temp'))
+  }
 
-function bom() {
-    // TODO
-    console.log('bom helper')
-}
+module.exports = {
+    openTab
+};
